431a6cdcf4f0d3b06346efef2c2880ae9a903069
J-Hernandez1 <jakehernandez397@gmail.com>
2024-07-26 17:26:01 +0100

    add request and styling

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,13 @@
 
 class HomeController < ApplicationController
+
+
     def index
-        
+       url = "https://api.openweathermap.org/data/2.5/weather?lat=51.509865&lon=-0.118092&units=metric&appid=992d118c3cca8e7cb6c071de74cee07d"
+
+       uri = URI(url)
+       res = Net::HTTP.get_response(uri)
+
+       @data = JSON.parse(res.body)
     end
 end
