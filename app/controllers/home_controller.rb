e6d293a2cd36914100f8c6e50f3f1865a28d8349
J-Hernandez1 <jakehernandez397@gmail.com>
2024-07-31 18:28:53 +0100

    tidy up index

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,7 @@
 
 class HomeController < ApplicationController
     def index
-       @data = CurrentWeatherService.new(latitude: "51.509865", longitude: "-0.118092", units: "metric").call
+       data = CurrentWeatherService.new(latitude: "51.509865", longitude: "-0.118092", units: "metric").call
+       @weather = Weather.new(data)
     end
 end
